dbd500cb896ed71d306e366085f4900bb109a56a
Sari Bäckman <sari.m.backman@gmail.com>
2023-03-21 13:12:19 +0200

    tehtävä 2.12

--- 2/puhelinluettelo/src/App.js
+++ 2/puhelinluettelo/src/App.js
@@ -59,12 +59,15 @@ const App = () => {
     }
     const personObject = {
       name: newName,
-      number: newNumber,
-      id: (persons.length+1)
+      number: newNumber
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
   return (
