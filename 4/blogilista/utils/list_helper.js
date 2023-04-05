5b8df0a131ac7d78d3ba3d16ad54474e4428ce01
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-05 21:02:13 +0300

    tehtävä 4.4

--- 4/blogilista/utils/list_helper.js
+++ 4/blogilista/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    const likes = blogs.map(b => b.likes)
+    if (likes.length === 0) {
+        return 0
+    } else {
+        const reducer = (sum, item) => {
+            return sum + item
+        }
+        return likes.reduce(reducer, 0)
+    }
+}
   
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
