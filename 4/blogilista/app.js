38cdc7f297e990b86124641a3ca9c1f12d313162
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-05 19:11:30 +0300

    tehtävä 4.2

--- 4/blogilista/app.js
+++ 4/blogilista/app.js
@@ -1,40 +1,16 @@
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
-const logger = require('./utils/logger')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogRouter = require('./controllers/blogs')
+const requestLogger = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(requestLogger)
+app.use('/api/blogs', blogRouter)
 
 module.exports = app
