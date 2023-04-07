2708f6f26e9433dd97dfe88e9ed98182167b13da
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-07 23:18:42 +0300

    tehtävä 4.8

---
+++ 4/blogilista/tests/blogs_api.test.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+      },
+      {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+      },
+      {
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+      }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[2])
+    await blogObject.save()
+})
+
+test('blogs returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('there are right amount of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+})
