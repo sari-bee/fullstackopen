ea724058de26be577459a807c70f7251c832685e
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-08 21:28:17 +0300

    tehtävä 4.10 tarkennusta

--- 4/blogilista/tests/blogs_api.test.js
+++ 4/blogilista/tests/blogs_api.test.js
@@ -1,37 +1,17 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const initialBlogs = [
-    {
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-      },
-      {
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-      },
-      {
-        title: "First class tests",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-        likes: 10,
-      }
-]
-
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
+    let blogObject = new Blog(helper.initialBlogs[0])
     await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
+    blogObject = new Blog(helper.initialBlogs[1])
     await blogObject.save()
-    blogObject = new Blog(initialBlogs[2])
+    blogObject = new Blog(helper.initialBlogs[2])
     await blogObject.save()
 })
 
@@ -43,25 +23,19 @@ test('blogs returned as json', async () => {
 })
 
 test('there is the right amount of blogs', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length)
+    const blogsAtEnd = await helper.blogsInDatabase()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 
 test('there is a field called id', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].id).toBeDefined()
+    const blogsAtEnd = await helper.blogsInDatabase()
+    expect(blogsAtEnd[0].id).toBeDefined()
 })
 
 test('when posting the amount of blogs grows by one', async () => {
-    blogObject = new Blog({
-        title: "React patterns 2",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 9,
-    })
-    await blogObject.save()
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    await helper.blogToAdd.save()
+    const blogsAtEnd = await helper.blogsInDatabase()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 })
 
 afterAll(async () => {
---
+++ 4/blogilista/tests/test_helper.js
@@ -0,0 +1,36 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+      },
+      {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+      },
+      {
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+      }
+]
+
+const blogToAdd = new Blog({
+    title: "React patterns 2",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 9,
+})
+
+const blogsInDatabase = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = { initialBlogs, blogToAdd, blogsInDatabase }
