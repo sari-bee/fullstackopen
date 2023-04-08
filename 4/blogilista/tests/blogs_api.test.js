070325950279b0f76821cd37e0d20ada03ec2b01
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-08 23:14:58 +0300

    tehtävät 2.13-2.14

--- 4/blogilista/tests/blogs_api.test.js
+++ 4/blogilista/tests/blogs_api.test.js
@@ -47,6 +47,28 @@ test('response to missing url is 400', async () => {
     await api.post('/api/blogs').send(helper.blogWithoutURL).expect(400)
 })
 
+test('blog deletion returns code 204 and number of blogs is reduced', async () => {
+    const blogsAtStart = await helper.blogsInDatabase()
+    const blogToDelete = blogsAtStart[0]
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+    const blogsAtEnd = await helper.blogsInDatabase()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length-1)
+})
+
+test('the number of likes in a blog can be increased by one', async () => {
+    const allBlogs = await helper.blogsInDatabase()
+    const blogToUpdate = allBlogs[0]
+    const updatedBlog = {
+        title: blogToUpdate.title,
+        author: blogToUpdate.author,
+        url: blogToUpdate.url,
+        likes: blogToUpdate.likes+1,
+    }
+    await api.put(`/api/blogs/${blogToUpdate.id}`).send(updatedBlog)
+    const allBlogsAtEnd = await helper.blogsInDatabase()
+    expect(allBlogsAtEnd[0].likes).toEqual(blogToUpdate.likes+1)
+})
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
