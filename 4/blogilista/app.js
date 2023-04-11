ad8fdf71b1035c13ed372ab29f8f2c005efce72f
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-11 10:53:04 +0300

    tehtävä 4.18

--- 4/blogilista/app.js
+++ 4/blogilista/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
@@ -15,6 +16,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
