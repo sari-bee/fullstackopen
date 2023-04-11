c6ce04637a972944b6b8f215b09d9f03ca2badf2
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-11 20:51:49 +0300

    tehtävä 5.2

--- 5/blogilista_frontend/src/App.js
+++ 5/blogilista_frontend/src/App.js
@@ -16,10 +16,19 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const alreadyLoggedUser = window.localStorage.getItem('loggedBlogUser')
+    if (alreadyLoggedUser) {
+      const user = JSON.parse(alreadyLoggedUser)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -27,6 +36,12 @@ const App = () => {
       setErrorMessage('wrong credentials')
       setTimeout(() => { setErrorMessage(null) }, 5000)
     }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.clear()
+    setUser(null)
   }
 
   const loginForm = () => {
@@ -51,6 +66,9 @@ const App = () => {
       <>
       <h2>blogs</h2>
       {user.name} logged in
+      <form onSubmit={handleLogout}>
+        <button type="submit">log out</button>
+      </form>
       <p></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
