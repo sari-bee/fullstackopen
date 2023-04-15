16583d9e1f7c12ae5cc28466d964bd6f394c6fa6
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-15 21:10:25 +0300

    tehtävä 5.10

--- 5/blogilista_frontend/src/App.js
+++ 5/blogilista_frontend/src/App.js
@@ -88,7 +88,7 @@ const App = () => {
         <AddBlogForm createBlog={createBlog}/>
       </Togglable>
       <p></p>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes-a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} addLike={addLike} />
       )}
       </>
