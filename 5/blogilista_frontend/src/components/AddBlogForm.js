3eb66be418f0b3a5b29a0afac31fcb5024c74db9
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-16 15:57:12 +0300

    tehtävä 5.16

--- 5/blogilista_frontend/src/components/AddBlogForm.js
+++ 5/blogilista_frontend/src/components/AddBlogForm.js
@@ -23,15 +23,15 @@ const AddBlogForm = ({ createBlog }) => {
       <h2>create new</h2>
       <form onSubmit={handleAdd}>
         <div>
-          title <input type="text" value={title} name="title" onChange={({ target }) => setTitle(target.value)}/>
+          title <input type="text" value={title} name="title" id="title-input" onChange={({ target }) => setTitle(target.value)}/>
         </div>
         <div>
-          author <input type="text" value={author} name="author" onChange={({ target }) => setAuthor(target.value)}/>
+          author <input type="text" value={author} name="author" id="author-input" onChange={({ target }) => setAuthor(target.value)}/>
         </div>
         <div>
-          url <input type="text" value={url} name="url" onChange={({ target }) => setUrl(target.value)}/>
+          url <input type="text" value={url} name="url" id="url-input"onChange={({ target }) => setUrl(target.value)}/>
         </div>
-        <button type="submit">create</button>
+        <button type="submit" id="submit-button">create</button>
       </form>
     </>
   )
