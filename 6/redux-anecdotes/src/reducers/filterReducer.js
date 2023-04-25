00a22d259be06f58169fb488d397f3f399f51aac
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-25 16:27:39 +0300

    tehtävä 6.10

--- 6/redux-anecdotes/src/reducers/filterReducer.js
+++ 6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,15 +1,14 @@
-const filterReducer = (state = '', action) => {
-    switch(action.type) {
-        case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit'
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState: '',
+    reducers: {
+        createFilter(state, action) {
             return action.payload
-        default: return state
-    }
-}
+        }
+    },
+})
 
-export const createFilter = (content) => { return {
-    type: 'SET_FILTER',
-    payload: content
-  }
-}
-
-export default filterReducer
+export const { createFilter } = filterSlice.actions
+export default filterSlice.reducer
