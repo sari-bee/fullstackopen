134a77a3c46b1134a87de0e190d03803570c7408
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-25 16:48:10 +0300

    tehtävä 6.12

---
+++ 6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState: 'Here be notifications',
+    reducers: {
+
+    },
+})
+
+export default notificationSlice.reducer
