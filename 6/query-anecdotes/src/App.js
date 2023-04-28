0b5f4b81c4d1b925c2a26b99eb7d9102cfd4008f
Sari Bäckman <sari.m.backman@gmail.com>
2023-04-28 21:46:01 +0300

    tehtävä 6.20

---
+++ 6/query-anecdotes/src/App.js
@@ -0,0 +1,46 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { useQuery } from 'react-query'
+import { getAnecdotes } from './requests'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const result = useQuery(
+    'anecdotes',
+    getAnecdotes,
+    { retry: false }
+  )
+  if (result.isLoading) {
+    return <div>please wait</div>
+  }
+  if (result.isError) {
+    return <div>anecdote service not available due to problems with server</div>
+  }
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
