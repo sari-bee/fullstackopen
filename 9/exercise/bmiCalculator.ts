5b3037827753665664dbf44d08edd9450b7ea3bf
Sari Bäckman <sari.m.backman@gmail.com>
2023-06-04 21:41:56 +0300

    tehtävä 9.1

---
+++ 9/exercise/bmiCalculator.ts
@@ -0,0 +1,15 @@
+const calculateBmi = (height: number, weight: number) => {
+    const bmi = weight/((height/100)**2);
+    if(bmi < 18.5) {
+        return("Underweight")
+    }
+    if(bmi > 29.9) {
+        return("Obese")
+    }
+    if(bmi > 24.9) {
+        return("Overweight")
+    }
+    return("Normal (healthy weight)");
+}
+
+console.log(calculateBmi(180,74))
