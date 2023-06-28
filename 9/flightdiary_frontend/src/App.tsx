240134d6689c7c8ae1a85bf8ec4955e1aeec1ae0
Sari Bäckman <sari.m.backman@gmail.com>
2023-06-28 22:48:35 +0300

    tehtävä 9.17

--- 9/flightdiary_frontend/src/App.tsx
+++ 9/flightdiary_frontend/src/App.tsx
@@ -5,20 +5,48 @@ interface Entry {
   id: number,
   date: string,
   weather: string,
-  visibility: string
+  visibility: string,
+  comment: string,
 }
 
 const App = () => {
   const [entries, setEntries] = useState<Entry[]>([]);
+  const [newDate, setNewDate] = useState('');
+  const [newWeather, setNewWeather] = useState('');
+  const [newVisibility, setNewVisibility] = useState('');
+  const [newComment, setNewComment] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:3001/api/diaries').then(response => {
+    axios.get<Entry[]>('http://localhost:3001/api/diaries').then(response => {
       setEntries(response.data);
     });
-  })
+  }, [])
+
+  const entryCreator = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    axios.post<Entry>('http://localhost:3001/api/diaries', { 
+      date: newDate,
+      weather: newWeather,
+      visibility: newVisibility,
+      comment: newComment
+    })
+    .then(response => { setEntries(entries.concat(response.data)) });
+    setNewDate('');
+    setNewWeather('');
+    setNewVisibility('');
+    setNewComment('')
+  };
 
   return (
     <div>
+      <h2>Add new entry</h2>
+      <form onSubmit={entryCreator}>
+        date <input value={newDate} onChange={(event) => setNewDate(event.target.value)}/><br/>
+        visibility <input value={newVisibility} onChange={(event) => setNewVisibility(event.target.value)}/><br/>
+        weather <input value={newWeather} onChange={(event) => setNewWeather(event.target.value)}/><br/>
+        comment <input value={newComment} onChange={(event) => setNewComment(event.target.value)}/><br/>
+        <button type='submit'>add</button>
+      </form>
       <h2>Diary entries</h2>
       {entries.map(entry =>
       <p key={entry.id}><b>{entry.date}</b><br/>visibility: {entry.visibility}<br/>weather: {entry.weather}</p>)}
